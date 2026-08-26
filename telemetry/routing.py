626996f102744cec22a73f47129a2d85ed7a3e78
bonheurNE07 <bonheurndezenc@gmail.com>
2026-08-26 12:50:50 +0200

    fix: fixed the asgi

--- telemetry/routing.py
+++ telemetry/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path, path
 from telemetry.consumers import TelemetryConsumer
 
 websocket_urlpatterns = [
-    path("ws/telemetry/equipment/<int:equipment_id>/", TelemetryConsumer.as_asgi()),
+    path("ws/telemetry/equipment/<str:equipment_id>/", TelemetryConsumer.as_asgi()),
     path("ws/telemetry/global/", TelemetryConsumer.as_asgi()),
     re_path(r"^ws/telemetry/?$", TelemetryConsumer.as_asgi()),
 ]
